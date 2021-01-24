d97b7ffe0cf5b23fe0788e2dc536430c413f4976
mohit.kumar <mohit.kumar@pantelwar.com>
2021-01-24 23:15:50 +0530

    JS Clock added along with Drum Kits

---
+++ jsclock/index.js
@@ -0,0 +1,22 @@
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+
+function setDate() {
+    const nowDate = new Date();
+
+    const seconds = nowDate.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const mins = nowDate.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+    const hour = nowDate.getHours();
+    const hourDegrees = ((hour / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
